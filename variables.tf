f1ca8c4e822fcb05a33b268a4a3efc6e01893c93
Snehit7 <37305534+snedsce@users.noreply.github.com>
2025-07-15 15:54:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,16 @@
 variable "region" {
   description = "Enter the region you want to provision your Infrastructure from : "
+  default = "us-east-1"
 }
 
 variable "ami" {
   description = "Enter the ami of Instance: "
+  default = "ami-020cba7c55df1f615"
 }
 
 variable "instance_type" {
   description = "Enter the Instance type: "
+  default = "t2.micro"
 }
 
 variable "cidr" {
@@ -18,4 +21,5 @@ variable "cidr" {
 
 variable "name" {
   description = "Enter the name of Instance: "
+  default = "ec2-test"
 }
